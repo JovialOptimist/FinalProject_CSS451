f2607a0aa23ecb8f42cf968e2b31ed32b46753f3
Jovialist <jac@thinkman.com>
2024-12-03 16:08:30 -0800

    Some script files
    They don't have anything in them

---
+++ Assets/Scripts/GenerateMountains_GradientPerlin_Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMountains_GradientPerlin_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
